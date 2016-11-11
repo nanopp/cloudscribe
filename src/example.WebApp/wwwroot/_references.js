34a1daa61fd3df10b67c065588d07c42584aedd3
Mariano Peinador <nanopp@gmail.com>
2016-11-11 14:46:16 -0600

    Added connection string MySql

--- src/example.WebApp/wwwroot/_references.js
+++ src/example.WebApp/wwwroot/_references.js
@@ -1,5 +1,6 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="app.js" />
 /// <reference path="app-two.js" />
+/// <reference path="js/site.min.js" />
 /// <reference path="jsdev/site.js" />
 /// <reference path="oidc-client.js" />
